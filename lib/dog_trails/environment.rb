ff551171fad0c1a482bd437822fd59b88212b318
Brittany Javalera <brittjavalera@gmail.com>
2019-07-31 23:35:37 +0000

    added require & require_relatives to environment

--- lib/dog_trails/environment.rb
+++ lib/dog_trails/environment.rb
@@ -1,2 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
-
+require_relative '../lib/dog_trails/scraper'
+require_relative '../lib/dog_trails/cli'
+require_relative '../lib/dog_trails/version'
+require_relative '../lib/dog_trails/trails'
