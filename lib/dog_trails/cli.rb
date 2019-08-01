c83a8306a6665f99f0b50492c6605fc589a39e2a
Brittany Javalera <brittjavalera@gmail.com>
2019-08-01 14:47:26 +0000

    added puts statements to each method in CLI class

--- lib/dog_trails/cli.rb
+++ lib/dog_trails/cli.rb
@@ -1,17 +1,19 @@
 class DogTrails::CLI
   def call
-   puts "Hello dog-lover! What region would you like to explore?"
   list_regions
   list_trail_names
   trail_specs
  end
  
 def list_regions
+  puts "Hello dog-lover! Please enter number of the region you would like to explore"
 end
  
 def list_trail_names
+  puts "Please enter the number of the trail you like more information about, or enter 'return to regions', or enter 'exit'" 
 end
  
 def trail_specs
+  puts "About this hike"
 end
 end
--- lib/dog_trails/trails.rb
+++ lib/dog_trails/trails.rb
@@ -1,2 +1,3 @@
 class DogTrails::Trails
+  attr_accessor :
 end
